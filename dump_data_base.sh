668daa522ddb9aa81261928a47dfd8cb9cb97938
Augusto Maza <debianmaza@gmail.com>
2022-08-17 15:46:02 -0300

    update #4 17/08/2022 arreglos generales

--- dump_data_base.sh
+++ dump_data_base.sh
@@ -2,7 +2,7 @@
 fecha=`date +%d-%m-%Y`
 archivo="gnu_expedientes-$fecha.sql"
 mysqldump --user=root --password=slack142 --host=localhost gnu_expedientes > $archivo
-#mysqldump --user=root --password=slack142 --host=slackzone.ddns.net mcsal > $archivo
+#mysqldump --user=gnu_exp --password=gnu_exp --host=localhost gnu_expedientes > $archivo
 chmod 777 $archivo
 mv $archivo sqls/
 

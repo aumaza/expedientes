80194b362ad25243e4ff830e9bb091320b4fd87c
Augusto Maza <debianmaza@gmail.com>
2022-09-02 18:41:19 -0300

    update #16 02/09/2022 arreglo general

--- connection/connection.php
+++ connection/connection.php
@@ -1,11 +1,11 @@
 <?php
 
-$dbhost = 'slackzone.ddns.net';
-//$dbhost = 'localhost';
-$dbuser = 'root';
-//$dbuser = 'gnu_exp';
-$dbpass = 'slack142';
-//$dbpass = 'gnu_exp';
+//$dbhost = 'slackzone.ddns.net';
+$dbhost = 'localhost';
+//$dbuser = 'root';
+$dbuser = 'gnu_exp';
+//$dbpass = 'slack142';
+$dbpass = 'gnu_exp';
 $dbase = 'gnu_expedientes';
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbase);
 

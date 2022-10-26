dd007aa3a30e35a85eb6fd1f16b9709450cc026c
Augusto Maza <debianmaza@gmail.com>
2022-10-26 15:55:41 -0300

    update #22 26/10/2022 update general

--- connection/connection.php
+++ connection/connection.php
@@ -1,21 +1,21 @@
 <?php
 
 // DATOS DE CONEXION LOCALHOST
-
+/*
 //$dbhost = 'slackzone.ddns.net';
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'slack142';
 $dbase = 'gnu_expedientes';
-
+*/
 
 // DATOS DE CONEXION PARA SERVER PRODUCCION
-/*
+
 $dbhost = 'localhost';
 $dbuser = 'gnu_exp';
 $dbpass = 'gnu_exp';
 $dbase = 'gnu_expedientes';
-*/
+
 
 $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbase);
 
